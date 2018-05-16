87bc5546bdbedcb59fcf79b3267b0ec3702bcbba
Mattia Alfieri <cikkense@gmail.com>
2018-05-16 08:54:13 +0200

    Fixed cp

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz
 tar xzvf node_exporter-0.16.0.linux-amd64.tar.gz
-cp node_exporter-0.16.0.linux-amd64.tar.gz/node_exporter /usr/bin
+cp node_exporter-0.16.0.linux-amd64/node_exporter /usr/bin
 chmod +x /usr/bin/node_exporter
 cp ./node_exporter.service /etc/systemd/system
 systemctl daemon-reload
